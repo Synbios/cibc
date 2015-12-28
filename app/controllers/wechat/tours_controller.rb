d58baab5eb08d0c3f0a7b6f6d3dbdb1a720452d2
Rah Zhou <youz@ualberta.ca>
2015-12-28 21:09:11 +0800

    use domain name instead of IP

--- app/controllers/wechat/tours_controller.rb
+++ app/controllers/wechat/tours_controller.rb
@@ -5,6 +5,7 @@ class Wechat::ToursController < ApplicationController
   end
 
   def show
+    @domain_url = "www.jhtcd.com"
     @wx_signature = wechat_generate_jsapi_signature request.url
     render "tours/huang", layout: "shijiebang"
   end
