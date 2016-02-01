92715f8091abc1df2920f1215ff3ce54e7c32776
Rah Zhou <youz@ualberta.ca>
2016-02-01 19:59:15 +0800

    fixed tour avatar controller error

--- app/controllers/wechat/tour_avators_controller.rb
+++ app/controllers/wechat/tour_avators_controller.rb
@@ -1,10 +1,10 @@
-class Wechat::ToursController < ApplicationController
+class Wechat::TourAvatorsController < ApplicationController
   include Wechat::MasterHelper
 
   def show
     @domain_url = "www.jhtwork.com"
-    @tour_avator = TourAvator.find_by_id params[:id]
-    @shards = @tour_avator.shards
+    @tour = TourAvator.find_by_id params[:id]
+    @shards = @tour.shards
     @wx_signature = wechat_generate_jsapi_signature request.url
     render layout: "shijiebang"
   end
