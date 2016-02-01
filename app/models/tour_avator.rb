f07634254ff70ed9007fe2f739d2fe0910496715
Rah Zhou <youz@ualberta.ca>
2016-02-01 19:16:28 +0800

    changed tmp directory

--- app/models/tour_avator.rb
+++ app/models/tour_avator.rb
@@ -12,10 +12,10 @@ class TourAvator < Tour
       self.shards.delete_all
 
       # slice original image into smaller images
-      system("convert #{original_path}  -crop 800x600  tmp/ta_#{self.id}_%d.jpg")
+      system("convert #{original_path}  -crop 800x600  #{Rails.root}/public/ta_#{self.id}_%d.jpg")
 
       # turn slice images into shards
-      file_paths = `ls tmp/ta_#{self.id}_*.jpg`.split("\n")
+      file_paths = `ls #{Rails.root}/public/ta_#{self.id}_*.jpg`.split("\n")
       file_paths.each do |file_path|
         shard = self.shards.new
         shard.order = /(\d+)\.jpg/.match(file_path).captures.first
@@ -26,7 +26,7 @@ class TourAvator < Tour
       end
 
       # remove tmp files
-      system("rm tmp/ta_#{self.id}_*.jpg")
+      system("rm #{Rails.root}/public/ta_#{self.id}_*.jpg")
     end
   end
 end
